561ce24df0c467a84daead49d9667fb8daa38061
Gustavo Pacheco <gustavo.pacheco.dev@gmail.com>
2023-03-23 14:50:51 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,20 @@
+import { HttpResponse, HttpRequest} from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-error'
+
 export class SignUpController {
-  handle (_httpRequest) {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name')
+  handle (httpRequest: HttpRequest): HttpResponse {
+    if(!httpRequest.body.name){
+      return {
+        statusCode: 400,
+        body: new MissingParamError('name')
+      }
     }
+    if(!httpRequest.body.email){
+      return {
+        statusCode: 400,
+        body: new MissingParamError('email')
+      }
+    }
+    return this.handle();
   }
 }
