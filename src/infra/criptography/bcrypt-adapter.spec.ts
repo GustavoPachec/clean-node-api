deed34e08c63e3df0be6b7043e857d87877fce06
Gustavo Pacheco <gustavo.pacheco.dev@gmail.com>
2023-04-26 00:01:35 -0300

    feat: ensure BcryptAdapter returns true when compare succeeds

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -43,4 +43,10 @@ describe('Bcrypt Adapter', () => {
     await sut.compare('any_value', 'any_hash');
     expect(compareHash).toBeCalledWith('any_value', 'any_hash');
   });
+
+  test('Should return true when compare succeds', async () => {
+    const sut = makeSut();
+    const isValid = await sut.compare('any_value', 'any_hash');
+    expect(isValid).toBe(true);
+  });
 });
