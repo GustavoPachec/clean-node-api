dde43124c5231a51de23332126972abb6b85f2c2
Gustavo Pacheco <gustavo.pacheco.dev@gmail.com>
2023-04-29 16:51:54 -0300

    feat: ensure DbAddAccount calls LoadAccountByEmailrRepository with correct email

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -2,3 +2,5 @@ export * from '../../../domain/usecases/add-account';
 export * from '../../../domain/models/account';
 export * from '../../protocols/criptography/hesher';
 export * from '../../protocols/db/account/add-account-repository';
+// eslint-disable-next-line import/no-cycle
+export * from '../../protocols/db/account/load-account-by-email-repository';
