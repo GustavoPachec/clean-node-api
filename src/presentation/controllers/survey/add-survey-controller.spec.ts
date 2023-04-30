da791dd7ed222eb23750babda442eb260fad9f62
Gustavo Pacheco <gustavo.pacheco.dev@gmail.com>
2023-04-30 13:54:32 -0300

    feat: ensure AddSurveyController returns 500 if AddSurveyUseCase throws

--- src/presentation/controllers/survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey-controller.spec.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line max-classes-per-file
 import { HttpRequest, AddSurveyModel, AddSurvey, Validation } from './add-survey-controller-protocols';
 import { AddSurveyController } from './add-survey-controller';
-import { badRequest } from '../../helpers/http/http-helper';
+import { badRequest, serverError } from '../../helpers/http/http-helper';
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -74,5 +74,13 @@ describe('AddSurvey Controller', () => {
     const httpRequest = makeFakeRequest();
     sut.handle(httpRequest);
     expect(addSpy).toBeCalledWith(httpRequest.body);
+  });
+
+  test('Should return 500 if AddSurvey throws', async () => {
+    const { sut, addSurveyStub } = makeSut();
+    // eslint-disable-next-line @typescript-eslint/no-shadow
+    jest.spyOn(addSurveyStub, 'add').mockImplementationOnce(() => new Promise((_resolve, reject) => reject(new Error())));
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(serverError(new Error()));
   });
 });
