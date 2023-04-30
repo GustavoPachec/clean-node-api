5f45e785186b2100ba938608ec1cc7f67d398b0a
Gustavo Pacheco <gustavo.pacheco.dev@gmail.com>
2023-04-30 12:02:13 -0300

    refactor: move login and signup controllers to a folder

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -6,13 +6,14 @@ import { Controller } from '../../presentation/protocols';
 import { LogControllerDecorator } from '../decorators/log-controller-decorator';
 import { LogMongoRepository } from '../../infra/db/mongodb/log/log-mongo-repository';
 import { makeSignUpValidation } from './controllers/signup/signup-validation-factory';
+import { makeDbAuthentication } from './usecases/authentication/db-authentication-factory';
 
 export const makeSignUpController = (): Controller => {
   const salt = 12;
   const bycrytAdapter = new BcryptAdapter(salt);
   const accountMongoRepository = new AccountMongoRepository();
   const dbAddAccount = new DbAddAccount(bycrytAdapter, accountMongoRepository);
-  const signuUpController = new SignUpController(dbAddAccount, makeSignUpValidation());
+  const signuUpController = new SignUpController(dbAddAccount, makeSignUpValidation(), makeDbAuthentication());
   const logMongoRepository = new LogMongoRepository();
   return new LogControllerDecorator(signuUpController, logMongoRepository);
 };
