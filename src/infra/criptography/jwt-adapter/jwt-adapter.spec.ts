3d510b8de480434dc3badc582eca4a11ec8277b3
Gustavo Pacheco <gustavo.pacheco.dev@gmail.com>
2023-05-02 18:10:14 -0300

    feat: ensure JwtAdapter call verify with correct values

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -3,7 +3,13 @@ import jwt from 'jsonwebtoken';
 import { JwtAdapter } from './jwt-adapter';
 
 jest.mock('jsonwebtoken', () => ({
-  sign: (): Promise<string> => new Promise((resolve) => resolve('any_token')),
+  async sign(): Promise<string> {
+    return new Promise((resolve) => resolve('any_token'));
+  },
+
+  async verify(): Promise<string> {
+    return new Promise((resolve) => resolve('any_value'));
+  },
 }));
 
 const makeSut = (): JwtAdapter => new JwtAdapter('secret');
@@ -30,6 +36,15 @@ describe('Jwt Adapter', () => {
       });
       const promise = sut.encrypt('any_id');
       await expect(promise).rejects.toThrow();
+    });
+  });
+
+  describe('verify()', () => {
+    test('Should call verify with correct values', async () => {
+      const sut = makeSut();
+      const verifySpy = jest.spyOn(jwt, 'verify');
+      await sut.decrypt('any_token');
+      expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret');
     });
   });
 });
