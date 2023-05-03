f5a96aa538ff954de005b1cb902296d113af20e2
Gustavo Pacheco <gustavo.pacheco.dev@gmail.com>
2023-05-03 14:37:23 -0300

    test: ensure add survey route returns 403 if no access token is provid

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,9 +1,12 @@
 import request from 'supertest';
 import { Collection } from 'mongodb';
+import { sign } from 'jsonwebtoken';
 import app from '../config/app';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
+import env from '../config/env';
 
 let surveyCollection: Collection;
+let accountCollection: Collection;
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -17,6 +20,8 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys');
     await surveyCollection.deleteMany({});
+    accountCollection = await MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
   });
 
   describe('POST /surveys', () => {
@@ -41,5 +46,46 @@ describe('Survey Routes', () => {
         });
       expect(403);
     });
+  });
+
+  test('Should return 204 on add survey with valid accessToken', async () => {
+    const { insertedId: id } = await accountCollection.insertOne({
+      name: 'Guga',
+      email: 'guga@gmail.com',
+      password: 'guga123',
+      role: 'admin',
+    });
+    const accessToken = sign({ id }, env.jwtSecret);
+    await accountCollection.updateOne(
+      {
+        _id: id,
+      },
+      {
+        $set: {
+          accessToken,
+        },
+      }
+    );
+
+    await request(app)
+      .post('/api/surveys')
+      .set('x-access-token', accessToken)
+      .send({
+        question: 'Question',
+        answers: [
+          {
+            answers: [
+              {
+                answer: 'Answers 1',
+                image: 'http://imagem.com',
+              },
+              {
+                answers: 'Answers 2',
+              },
+            ],
+          },
+        ],
+      });
+    expect(204);
   });
 });
