0b95979b19785260f649614818773d1d95173401
Gustavo Pacheco <gustavo.pacheco.dev@gmail.com>
2023-05-04 09:55:23 -0300

    feat: ensure LoadSurveysController returns 200 on success

--- src/presentation/controllers/load-surveys/load-surveys-controller.spec.ts
+++ src/presentation/controllers/load-surveys/load-surveys-controller.spec.ts
@@ -1,12 +1,12 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import MockDate from 'mockdate';
-import { SurveyModel, LoadSurveys } from './load-surveys-controller-protocols';
 import { LoadSurveysController } from './load-surveys-controller';
+import { SurveyModel, LoadSurveys } from './load-surveys-controller-protocols';
+import { ok } from '../../helpers/http/http-helper';
 
 const makeFakeSurveys = (): SurveyModel[] => [
   {
     id: 'any_id',
-    question: 'any_question',
+    question: 'any_questions',
     answers: [
       {
         image: 'any_image',
@@ -15,9 +15,10 @@ const makeFakeSurveys = (): SurveyModel[] => [
     ],
     date: new Date(),
   },
+
   {
     id: 'other_id',
-    question: 'other_question',
+    question: 'other_questions',
     answers: [
       {
         image: 'other_image',
@@ -27,7 +28,28 @@ const makeFakeSurveys = (): SurveyModel[] => [
     date: new Date(),
   },
 ];
+interface SutTypes {
+  sut: LoadSurveysController;
+  loadSurveysStub: LoadSurveys;
+}
 
+const makeLoadSurveys = (): LoadSurveys => {
+  class LoadSurveysStub implements LoadSurveys {
+    async load(): Promise<SurveyModel[]> {
+      return new Promise((resolve) => resolve(makeFakeSurveys()));
+    }
+  }
+  return new LoadSurveysStub();
+};
+
+const makeSut = (): SutTypes => {
+  const loadSurveysStub = makeLoadSurveys();
+  const sut = new LoadSurveysController(loadSurveysStub);
+  return {
+    sut,
+    loadSurveysStub,
+  };
+};
 describe('LoadSurveys Controller', () => {
   beforeAll(() => {
     MockDate.set(new Date());
@@ -37,16 +59,16 @@ describe('LoadSurveys Controller', () => {
     MockDate.reset();
   });
 
-  test('Shuold call LoadSurveys', async () => {
-    class LoadSurveysStub implements LoadSurveys {
-      async load(): Promise<SurveyModel[]> {
-        return new Promise((resolve) => resolve(makeFakeSurveys()));
-      }
-    }
-    const loadSurveysStub = new LoadSurveysStub();
+  test('Shold call LoadSurveys', async () => {
+    const { sut, loadSurveysStub } = makeSut();
     const loadSpy = jest.spyOn(loadSurveysStub, 'load');
-    const sut = new LoadSurveysController(loadSurveysStub);
     await sut.handle({});
     expect(loadSpy).toHaveBeenCalled();
+  });
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut();
+    const httResponse = await sut.handle({});
+    expect(httResponse).toEqual(ok(makeFakeSurveys()));
   });
 });
